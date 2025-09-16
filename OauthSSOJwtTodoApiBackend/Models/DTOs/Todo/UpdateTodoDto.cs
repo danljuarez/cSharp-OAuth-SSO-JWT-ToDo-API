ef6356b1c3edbaf830914800cd1e1644f176cb9c
daniel <daniel_jc2005@outlook.com>
2025-09-16 11:52:27 -0400

    Allow Title to be null.

--- OauthSSOJwtTodoApiBackend/Models/DTOs/Todo/UpdateTodoDto.cs
+++ OauthSSOJwtTodoApiBackend/Models/DTOs/Todo/UpdateTodoDto.cs
@@ -5,7 +5,7 @@
 /// </summary>
 public class UpdateTodoDto
 {
-    public string Title { get; set; } = string.Empty;
+    public string? Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public bool? IsCompleted { get; set; }
 }
